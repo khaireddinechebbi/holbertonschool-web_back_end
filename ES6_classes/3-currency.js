232480c112e88ce952b03b1950df6889445e6aa7
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-07-21 12:06:02 +0200

    currency

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,35 @@
+export default class Currency {
+  constructor(code, name) {
+    if (typeof code !== 'string') {
+      throw new TypeError('code must be a string');
+    }
+    this._code = code;
+
+    if (typeof name !== 'string') {
+      throw new TypeError('name must be a string');
+    }
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(newcode) {
+    if (typeof code !== 'string') {
+      throw new TypeError('code must be a string');
+    }
+    this._code = newcode;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newname) {
+    if (typeof name !== 'string') {
+      throw new TypeError('name must be a string');
+    }
+    this._name = name;
+  }
+}
