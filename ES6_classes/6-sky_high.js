84596297f09c3e069aeaf8da7f39bbfee87153dd
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-07-21 13:45:48 +0200

    this._sqft = sqft;

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -3,10 +3,7 @@ import Building from './5-building';
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-
-    if (typeof floors !== 'number') {
-        throw new TypeError('Floors must be a number');
-    }
+    this._sqft = sqft;
     this._floors = floors;
   }
 
