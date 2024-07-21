777c8e4883abf578ecdc0b3d645e921192f20df4
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-07-21 11:38:06 +0200

    hbtn courses

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,51 @@
+export default class HolbertonCourse {
+    constructor(name, lenght, students) {
+        if (typeof name !== 'string') {
+            throw new TypeError('Name must be a string');
+        }
+        this._name = name;
+
+        if (typeof lenght !== 'number') {
+            throw new TypeError('Lenght must be a number');
+        }
+        this._lenght = lenght;
+
+        if (!Array.isArray(students) || !students.every(student => typeof student === 'string')) {
+            throw new TypeError('Students must be an array of strings');
+        }
+        this._students = students;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(newname) {
+        if (typeof newname !== 'string') {
+            throw new TypeError('Name must be a string');
+        }
+        this._name = newname;
+    }
+
+    get lenght() {
+        return this._lenght;
+    }
+
+    set lenght(newlenght) {
+        if (typeof newlenght !== 'number') {
+            throw new TypeError('Lenght must be a nember');
+        }
+        this._lenght = newlenght;
+    }
+
+    get students() {
+        return this._students;
+    }
+
+    set students(newstudents) {
+        if (!Array.isArray(newstudents) || !newstudents.every(student => typeof student === 'string')) {
+            throw new TypeError('Students must be an array of srtings');
+        }
+        this._students = newstudents;
+    }
+}
