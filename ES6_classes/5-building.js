02f5697000284a77e34c807d7c13c96883e53db4
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-07-21 13:09:21 +0200

    implementing corrections

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -3,6 +3,11 @@ export default class Building {
     if (new.target === Building) {
       throw new TypeError('Abstract class "Building" cannot be instantiated directly');
     }
+
+    if (typeof sqft !== 'number') {
+        throw new TypeError('Square footage (sqft) must be a number');
+    }
+
     this._sqft = sqft;
   }
 
