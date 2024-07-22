0a373eaef3ce54c3c5bcf83d1e12dffd9a6cbd86
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-07-22 12:15:00 +0200

    eslint corrections

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,13 +1,13 @@
 export default class car {
-    constructor(brand, motor, color) {
-        this._brand = brand;
-        this._motor = motor;
-        this._color = color;
-    }
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
 
-    cloneCar() {
-        const constructor = Object.getPrototypeOf(this).constructor;
-        const newCar = new constructor(this._brand, this._motor, this._color);
-        return newCar;
-    }
+  cloneCar() {
+    const { constructor } = Object.getPrototypeOf(this);
+    const newCar = new constructor(this._brand, this._motor, this._color);
+    return newCar;
+  }
 }
