898e47a0d9c3832531112fe6d1ab873821d6ead0
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-07-23 12:20:39 +0200

    array of objects

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -3,5 +3,5 @@ export default function getListStudents() {
         {id: 1, firstname: 'Guillaume', location: 'San Francisco' },
         { id: 2, firstName: 'James', location: 'Columbia' },
         { id: 5, firstName: 'Serena', location: 'San Francisco' }
-    ]
+    ];
 }
