b586cd4899184e289298e66429fd07622fccac0e
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-07-23 12:47:19 +0200

    filter

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,9 +1,3 @@
 export default function getStudentByLocation(students, city) {
-  if (!Array.isArray(students)) {
-    return [];
-  }
-  if (students.filter((student) => student.location !== city)) {
-    return [];
-  }
   return students.filter((student) => student.location === city);
 }
