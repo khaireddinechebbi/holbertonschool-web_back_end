c3102ece23c44f55fad875d1c1c68c5899b9b09b
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-07-23 13:58:07 +0200

    corrections

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,8 +1,9 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  return students.filter((student) => student.location === city).map((student) => {
-    const GradeObject = newGrades.find((grade) => grade.studentID = student.id);
-    return {
-      ...student, grade: GradeObject ? GradeObject.grade : 'N/A',
-    };
-  });
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const GradeObject = newGrades.find((grade) => grade.studentID === student.id);
+      const grade = GradeObject ? GradeObject.grade : 'N/A';
+      return { ...student, grade };
+    });
 }
