e5898cf644cc53e8b58ca5d1959f7a5cb4615184
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-07-25 14:44:35 +0200

    correction (value, key)

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -2,7 +2,7 @@ export default function updateUniqueItems(map) {
   if (!(map instanceof Map)) {
     throw new Error('Cannot process');
   }
-  map.forEach((key, value) => {
+  map.forEach((value, key) => {
     if (value === 1) {
       map.set(key, 100);
     }
