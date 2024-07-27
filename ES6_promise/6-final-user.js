d7002afa59216b5c33ab39fba9d14947678e81d8
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-07-28 01:50:07 +0200

    .map

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -5,7 +5,7 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const user = signUpUser(firstName, lastName);
   const photo = uploadPhoto(fileName);
   const promises = [user, photo];
-  return Promise.allSettled(promises).then((results) => results.forEach((result) => ({
+  return Promise.allSettled(promises).then((results) => results.map((result) => ({
     status: result.status,
     value: result.status === 'fulfilled' ? result.value : result.reason,
   })));
