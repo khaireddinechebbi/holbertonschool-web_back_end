e5736c07c3b14c3cf500a77809509f9957311a94
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-07-28 01:58:37 +0200

    corrections

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,13 +2,25 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const userPromise = signUpUser(firstName, lastName);
-  const photoPromise = uploadPhoto(fileName);
+  console.log('Starting handleProfileSignup');
 
-  return Promise.allSettled([userPromise, photoPromise]).then((results) => {
-    return results.map((result) => ({
+  const user = signUpUser(firstName, lastName);
+  const photo = uploadPhoto(fileName);
+  const promises = [user, photo];
+
+  console.log('Promises created:', promises);
+
+  return Promise.allSettled(promises).then((results) => {
+    console.log('All promises settled:', results);
+
+    const finalResults = results.map((result) => ({
       status: result.status,
       value: result.status === 'fulfilled' ? result.value : result.reason,
     }));
+
+    console.log('Final results:', finalResults);
+    return finalResults;
+  }).catch((error) => {
+    console.error('Error in handleProfileSignup:', error);
   });
 }
