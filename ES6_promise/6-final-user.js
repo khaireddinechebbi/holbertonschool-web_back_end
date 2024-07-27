519199d44455e3380ed49aa58c4396fbf460a231
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-07-28 01:47:18 +0200

    final user

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,12 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const user = signUpUser(firstName, lastName);
+  const photo = uploadPhoto(fileName);
+  const promises = [user, photo];
+  return Promise.allSettled(promises).then((results) => results.forEach((result) => ({
+    status: result.status,
+    value: result.value,
+  })));
+}
