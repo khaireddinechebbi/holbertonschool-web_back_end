5965cc1451b5bf9c6bfa28a1ab9c15bdeddf54d7
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-07-27 13:58:24 +0200

    Promise.all, console

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,9 +1,10 @@
-import { uploadPhoto, createUser } from "./utils";
+import { uploadPhoto, createUser } from './utils';
+
 export default function handleProfileSignup() {
-    return Promise.all([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser()])
     .then((results) => {
-        const [photo, user] = results;
-        console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+      const [photo, user] = results;
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
     .catch(() => console.log('Signup system offline'));
 }
