b347f1df4cd1b3afb52daa5506209024b1678b37
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-07-28 02:17:15 +0200

    final user

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,25 +2,14 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  console.log('Starting handleProfileSignup');
+  const signUpPromise = signUpUser(firstName, lastName);
+  const uploadPhotoPromise = uploadPhoto(fileName);
 
-  const user = signUpUser(firstName, lastName);
-  const photo = uploadPhoto(fileName);
-  const promises = [user, photo];
-
-  console.log('Promises created:', promises);
-
-  return Promise.allSettled(promises).then((results) => {
-    console.log('All promises settled:', results);
-
-    const finalResults = results.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
+  return Promise.allSettled([signUpPromise, uploadPhotoPromise])
+    .then((results) => results.map((result) => {
+      if (result.status === 'fulfilled') {
+        return { status: result.status, value: result.value };
+      }
+      return { status: result.status, value: result.reason.toString() };
     }));
-
-    console.log('Final results:', finalResults);
-    return finalResults;
-  }).catch((error) => {
-    console.error('Error in handleProfileSignup:', error);
-  });
 }
