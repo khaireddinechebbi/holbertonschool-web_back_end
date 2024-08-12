b8b01186a362fb2b87ed5011696f9987807695ea
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-08-12 17:54:29 +0200

    documentation

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python3
+"""
+This script take to floats and returns their sum as a float
+"""
 def add(a: float, b: float) -> float:
+    """
+    Returns the sum of two floats.
+
+    Parameters:
+    a (float): The first float.
+    b (float): The second float.
+
+    Returns:
+    float: The sum of the two floats.
+    """
     return a + b
