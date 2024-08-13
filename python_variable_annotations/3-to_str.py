3b5aa94a06bdfbbb61da51021072e41a1ead7e9a
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-08-13 15:55:26 +0200

    to str

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+This script defines a function that returns the string representation of a float.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Returns the string representation of a float.
+
+    Parameters:
+    n (float): The float value to convert to a string.
+
+    Returns:
+    str: The string representation of the float.
+    """
+    return str(n)
