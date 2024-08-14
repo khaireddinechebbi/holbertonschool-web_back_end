1335a39237dc0375eed0e4e739f03f556e6cfb48
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-08-14 09:30:23 +0200

    pycodestyle

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -6,6 +6,8 @@ The second element is the square of the int/float v and should be \
 annotated as a float.
 """
 from typing import Union, Tuple
+
+
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     Returns a tuple where the first element is the string k \
@@ -20,4 +22,4 @@ def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     Tuple[str, float]: A tuple containing the string k \
     and the square of v as a float.
     """
-    retun (k, float(v**2))
+    retun(k, float(v**2))
