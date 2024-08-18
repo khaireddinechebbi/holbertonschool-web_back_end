147faf4cbd4925668e52f218a37d572b306a5a74
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-08-18 10:52:53 +0200

    import corrections

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -9,7 +9,8 @@ in ascending order.
 """
 import asyncio
 from typing import List
-from 0-basic_async_syntax import wait_random
+wait_random = __import__("0-basic_async_syntax").wait_random
+
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """
@@ -26,5 +27,5 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     for _ in range(n):
         delay = await wait_random(max_delay)
         delays.append(delay)
-    
-    return sorted(delays)
+
+    return delays
