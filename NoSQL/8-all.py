d257f1c3b2c3c8850879468a064bc6efd084111e
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-08-25 12:24:03 +0200

    all

---
+++ NoSQL/8-all.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Lists all documents in a collection.
+"""
+
+from typing import List, Dict
+from pymongo.collection import Collection
+
+def list_all(mongo_collection: Collection) -> List[Dict]:
+    """
+    Lists all documents in a MongoDB collection.
+
+    Args:
+        mongo_collection (Collection): The pymongo collection object.
+
+    Returns:
+        List[Dict]: A list of dictionaries representing all documents in the collection.
+    """
+    return list(mongo_collection.find())
