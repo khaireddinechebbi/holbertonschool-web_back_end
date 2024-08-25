1a63a4815b3dd5a9180fbda870db2b4324da9e7d
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-08-25 14:53:58 +0200

    update

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -9,9 +9,9 @@ def update_topics(mongo_collection, name, topics):
     Updates the topics of a school document in a MongoDB collection based on the school name.
 
     Args:
-        mongo_collection (Collection): The pymongo collection object.
-        name (str): The name of the school to update.
-        topics (List[str]): The list of topics to set for the school.
+        mongo_collection: The pymongo collection object.
+        name: The name of the school to update.
+        topics: The list of topics to set for the school.
 
     Returns:
         None
