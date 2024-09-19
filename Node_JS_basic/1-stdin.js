55251bbd5994b214d0fed4a6c5d039de838dca2b
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-09-19 11:54:48 +0200

    process.stdin.on

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,7 +1,10 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
-  console.log(`Your name is: ${name}\r\n`);
-  process.stdout.write("This important software is now closing\r\n");
-  process.exit();
+  process.stdout.write(`Your name is: ${name}\n`);
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
